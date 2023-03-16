6347505dc70f34ea23173d69421a63e82df39569
GeeorgeUK <geeorge.uk@gmail.com>
2023-03-16 14:46:33 +0000

    Bumped version

--- server.lua
+++ server.lua
@@ -4,7 +4,7 @@ GlobChannel = 8190
 Modem = peripheral.find("modem")
 Modem.open(GlobChannel)
 -- The current network version.
-Version = {1,0,3}
+Version = {1,0,4}
 -- A log of messages.
 Log = {}
 -- All data about the network.
